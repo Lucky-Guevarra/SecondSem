932e76b6b0c597d282fb722083ec879ffd119eae
Lucky Angel Santos Guevarra <143337332+Lucky-Guevarra@users.noreply.github.com>
2024-03-15 18:22:51 +0800

    Update Guevarra_midLA2.py

--- Guevarra_midLA2.py
+++ Guevarra_midLA2.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import *
+from tkinter.messagebox import askyesno
 from PIL import ImageTk, Image
 
-class Cars(tk.Tk):
+class Cars(Tk):
     def __init__(self):
         super().__init__()
         self.title("J1T-midLA2")
@@ -43,11 +44,16 @@ class Cars(tk.Tk):
 
         self.mainfrm.pack()
         self.car_frm.pack()
-        self.bmw_frm.pack(side=LEFT)
-        self.subaru_frm.pack(side = LEFT)
-        self.chev_frm.pack(side=LEFT)
+        self.bmw_frm.pack(side='left')
+        self.subaru_frm.pack(side = 'left')
+        self.chev_frm.pack(side="left")
+        
+
     def upgrades(self):
-        upgrades_window = Upgrade(self.cars)
+        self.upgrades_window = Upgrade(self.cars)
+
+    
+
 
 class Upgrade(tk.Toplevel):
     def __init__(self, cars):
@@ -56,65 +62,83 @@ class Upgrade(tk.Toplevel):
         print(cars)
         self.geometry('500x500')
         self.title('Upgrades')
-        self.mainfrm = Frame(self)
-        self.interior_frm = Frame(self.mainfrm)
-        self.interiorfrmlblleft_frm = Frame(self.interior_frm)
-        self.interiorfrmlblright_frm = Frame(self.interior_frm)
-        self.interiorfrmchk_frm = Frame(self.interior_frm)
-        self.exteriorfnsh_frm = Frame(self.mainfrm)
-        self.exteriorfrmlblleft_frm = Frame(self.exteriorfnsh_frm)
-        self.exteriorfrmlblright_frm = Frame(self.exteriorfnsh_frm)
-        self.exteriorfrmrbd_frm = Frame(self.exteriorfnsh_frm)
-        self.exterioroptn_frm = Frame(self.mainfrm)
-        self.exteriorfrmoptnlblleft_frm = Frame(self.exterioroptn_frm)
-        self.exteriorfrmoptnlblright_frm = Frame(self.exterioroptn_frm)
-        self.interiorfrmoptnchk_frm = Frame(self.exterioroptn_frm)
+        self.configure(bg='#f6e0b5')
+        self.mainfrm = Frame(self, bg="#f6e0b5")
+        self.interior_frm = Frame(self.mainfrm, bg="#aa6f73", borderwidth=1,highlightbackground="#9aa0a3", highlightthickness=2,pady=50)
+        self.interiorfrmlbltop_frm = Frame(self.interior_frm)
+        self.interiorfrmlblbot_frm = Frame(self.interior_frm)
+        self.exteriorfnsh_frm = Frame(self.mainfrm, bg="#aa6f73", borderwidth=10, highlightbackground='#9aa0a3', highlightthickness=2, pady=25)
+        self.exteriorfrmlbltop_frm = Frame(self.exteriorfnsh_frm)
+        self.exteriorfrmlblbot_frm = Frame(self.exteriorfnsh_frm)
+        self.exterioroptn_frm = Frame(self.mainfrm, bg="#aa6f73", highlightbackground='#9aa0a3', highlightthickness=2, pady=25)
+        self.exteriorfrmoptnlbltop_frm = Frame(self.exterioroptn_frm)
+        self.exteriorfrmoptnlblbot_frm = Frame(self.exterioroptn_frm)
 
-        self.price1,self.price2,self.price3 = IntVar(),IntVar(),IntVar()
+        self.price1,self.price2,self.price3,self.price4,self.price5 = IntVar(),IntVar(),IntVar(),IntVar(),IntVar()
 
         upgr_list = (("leather", 550, -550),("GPS", 1000, -1000))
 
-        self.interior1_lbl = Label(self.interiorfrmlblleft_frm, text="Interior Options")
-        self.interior_lbl = Label(self.interiorfrmlblright_frm, text="leather")
-        self.interior_chk = Checkbutton(self.interiorfrmchk_frm, variable=self.price1, onvalue= 550, offvalue= -550)
-        self.interior2_lbl = Label(self.interiorfrmlblright_frm, text="GPS")
-        self.interior2_chk = Checkbutton(self.interiorfrmchk_frm, variable=self.price2, onvalue= 1000, offvalue= -1000)
+        self.interior1_lbl = Label(self.interiorfrmlbltop_frm, text="Interior Options", bg="#aa6f73")
+        self.interior_lbl = Label(self.interiorfrmlblbot_frm, text="leather", bg="#aa6f73")
+        self.interior_chk = Checkbutton(self.interiorfrmlblbot_frm, variable=self.price1, onvalue= 550, offvalue= -550)
+        self.interior2_lbl = Label(self.interiorfrmlblbot_frm, text="GPS", bg="#aa6f73")
+        self.interior2_chk = Checkbutton(self.interiorfrmlblbot_frm, variable=self.price2, onvalue= 1000, offvalue= -1000)
 
-        self.exterior1_lbl_frm = Label(self.exteriorfrmlblleft_frm, text="Exterior Finish")
-        self.exterior_lbl = Label(self.exteriorfrmlblright_frm, text="Hard Top")
-        self.exterior_rdb = Radiobutton(self.exteriorfrmrbd_frm, variable=self.price2, value=0)
-        self.exterior2_lbl = Label(self.exteriorfrmlblright_frm, text="Convertible")
-        self.exterior2_rdb = Radiobutton(self.exteriorfrmrbd_frm, variable=self.price2, value=0)
+        self.exterior1_lbl_frm = Label(self.exteriorfrmlbltop_frm, text="Exterior Finish", bg="#aa6f73")
+        self.exterior_lbl = Label(self.exteriorfrmlblbot_frm, text="Hard Top", bg="#aa6f73")
+        self.exterior_rdb = Radiobutton(self.exteriorfrmlblbot_frm, variable=self.price3, value=0)
+        self.exterior2_lbl = Label(self.exteriorfrmlblbot_frm, text="Convertible", bg="#aa6f73")
+        self.exterior2_rdb = Radiobutton(self.exteriorfrmlblbot_frm, variable=self.price3, value=0)
 
+        self.exterioroptn_lbl = Label(self.exteriorfrmoptnlbltop_frm, text="Exterior Option", bg="#aa6f73")
+        self.exterioroptn1_lbl = Label(self.exteriorfrmoptnlblbot_frm, text="Wheel Upgrade", bg="#aa6f73")
+        self.exterioroptn_chk = Checkbutton(self.exteriorfrmoptnlblbot_frm, variable=self.price4, onvalue=1000, offvalue=-1000)
+        self.exterioroptn2_lbl = Label(self.exteriorfrmoptnlblbot_frm, text="Performance Package", bg="#aa6f73")
+        self.exterioroptn2_chk = Checkbutton(self.exteriorfrmoptnlblbot_frm, variable=self.price5, onvalue=2000, offvalue=-2000)
 
         self.mainfrm.pack()
-        self.interior_frm.pack()
-        self.interiorfrmlblleft_frm.pack(side=LEFT)
-        self.interiorfrmlblright_frm.pack()
-        self.interiorfrmchk_frm.pack()
-        self.exteriorfnsh_frm.pack()
-        self.exteriorfrmlblleft_frm.pack(side=LEFT)
-        self.exteriorfrmlblright_frm.pack()
-        self.exteriorfrmrbd_frm.pack()
+        self.interior_frm.pack(padx=50)
+        self.interiorfrmlbltop_frm.pack(side="top")
+        self.interiorfrmlblbot_frm.pack(side='bottom')
+        self.exteriorfnsh_frm.pack(padx=50)
+        self.exteriorfrmlbltop_frm.pack(side="top")
+        self.exteriorfrmlblbot_frm.pack(side='bottom')
         self.exterioroptn_frm.pack()
-        self.exteriorfrmoptnlblleft_frm.pack(side=LEFT)
-        self.exteriorfrmoptnlblright_frm.pack()
-        self.interiorfrmoptnchk_frm.pack()
+        self.exteriorfrmoptnlbltop_frm.pack(side="top")
+        self.exteriorfrmoptnlblbot_frm.pack(side="bottom")
 
         self.interior1_lbl.pack(padx=100)
-        self.interior2_lbl.pack(side=RIGHT)
-        self.interior_lbl.pack(side=RIGHT)
-        self.interior2_chk.pack(side=RIGHT)
-        self.interior_chk.pack(side=RIGHT)
-        
+        self.interior2_lbl.pack(side="right")
+        self.interior2_chk.pack(side="right")
+        self.interior_chk.pack(side="right")
+        self.interior_lbl.pack(side="right")
+
+        self.exterior1_lbl_frm.pack(padx= 100)
+        self.exterior_lbl.pack(side='right')
+        self.exterior_rdb.pack(side='right')
+        self.exterior2_rdb.pack(side='right')
+        self.exterior2_lbl.pack(side='right')
+
+        self.exterioroptn_lbl.pack(padx=100)
+        self.exterioroptn1_lbl.pack(side='right')
+        self.exterioroptn_chk.pack(side='right')
+        self.exterioroptn2_chk.pack(side='right')
+        self.exterioroptn2_lbl.pack(side='right')
 
 
+        self.protocol("WM_DELETE_WINDOW", self.confirm)
         self.transient(self.master)
         self.grab_set()
         self.wait_window(self)
+        
 
+    def confirm(self):
+        print("confirmed")
+        self.destroy()
+
+    
 
 
 if __name__ == "__main__":
-    car_upgrade = Cars()
-    car_upgrade.mainloop()
+    main_wdw = Cars()
+    main_wdw.mainloop()
